42cc17cf52e6f27ab9eb8a44646ac509fbe77330
s72817 <lambo_1@gmx.de>
2015-02-23 09:32:05 +0100

    some changes

--- Menu/Menu/Menu_dyn.c
+++ Menu/Menu/Menu_dyn.c
@@ -4,53 +4,9 @@
 //#include "menu.c"
 #include "menu.c"
 
-//typedef struct _menu{
-//	char item[20];
-//	int level;
-//	struct _menu *next;
-//}menu;
-//
-//
-//void menu_add(menu **list, char *item, int level){
-//
-//	menu *newEl;
-//	menu *lst_iter = *list;
-//
-//	newEl = (menu*) malloc(sizeof(*newEl)); // erzeuge ein neues Element
-//	strcpy(newEl->item, item);
-//	newEl->level = level;
-//	newEl->next = NULL; // Wichtig für das Erkennen des Listenendes
-//
-//	if (lst_iter != NULL) { // sind Elemente vorhanden
-//		while (lst_iter->next != NULL) // suche das letzte Element
-//			lst_iter = lst_iter->next;
-//		lst_iter->next = newEl; // Hänge das Element hinten an
-//	}
-//	else // wenn die liste leer ist, bin ich das erste Element
-//		*list = newEl;
-//}
-//
-//void menu_show(menu *list, int level) {
-//	int counter = 1;
-//	while (list != NULL) {
-//		if (list->level == level)
-//			printf("%d. %s\n", counter++, list->item);
-//		list = list->next;
-//	}
-//}
-
 
 int main() {
-	//menu *m = NULL; // init. die Liste mit NULL = leere liste
-	menu *m = NULL;
-	
-	/*char *menu1[] = { 
-		"Kunde anlegen", 
-		"Anmelden", 
-		"Beenden"};
-	int len = 3;
-	for (int i = 0; i < len; i++)
-		menu_add(&m, menu1[i], 1);*/
+	menu *m = NULL; // init. die Liste mit NULL = leere liste
 
 	menu_add(&m, "Kunde anlegen",1);
 	menu_add(&m, "Anmelden", 1);
